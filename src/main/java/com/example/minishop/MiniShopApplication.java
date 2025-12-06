eaf9d93afffb778bba6e92b18c5dfefca5799c08
ReimoSi <122689064+ReimoSi@users.noreply.github.com>
2025-12-07 00:40:07 +0200

    fix: rename main class to MiniShopApplication to match filename

--- src/main/java/com/example/minishop/MiniShopApplication.java
+++ src/main/java/com/example/minishop/MiniShopApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.minishop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class MiniShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(MiniShopApplication.class, args);
 	}
 
 }
