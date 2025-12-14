c91ae7bf32789bc9f116081d0169bb73c785b986
ReimoSi <122689064+ReimoSi@users.noreply.github.com>
2025-12-14 16:25:52 +0200

    Added Swagger

---
+++ src/main/java/com/example/minishop/infrastructure/openapi/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.minishop.infrastructure.openapi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI minishopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MiniShop API")
+                        .description("Inventory & Catalog API (MVP)")
+                        .version("0.0.1"));
+    }
+}
